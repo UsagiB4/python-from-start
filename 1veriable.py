a6e3162f1ff1698f6e59b3b0dff3c73c89549fb3
UsagiB4 <binaryBunny@tutanota.com>
2021-07-19 22:21:13 +0600

    Variable

--- 1veriable.py
+++ 1veriable.py
@@ -11,15 +11,20 @@ name = "philosophy"
 number = 12
 point = 3.14159
 
-#_________________________________
+#___________Variable Type______________________
 print(type(name))  # this will print the type of variable "name"
 print(type(number))  # this will print the type of variable "number"
 print(type(point))  # this will print the type of variable "Point"
 print("_______________________________")  # this is just a line to separate the outputs
-#__________________________________
+#_____________Variable Slicing_____________________
 slice_name = name[1:5]
 print(slice_name)  # This will print from characters from index 1 to index 4
 nth_char = name[5]
 print(nth_char)  # This will print the character in index 4
 neg_index = name[-1]
 print(neg_index)  # This will print the first character from the end
+#_______________Multiple Value in Variable______________
+x, y, z = 12, 4, 5  # here we are assigning multiple value in multiple variable in a single line
+names = ['tim', 'bob', 'Jack']
+s, t, r = names  # this will assign the items in list as value of the variables.
+
