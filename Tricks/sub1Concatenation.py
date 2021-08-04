1654110719b23faedeca7911c1acbab0a6ea01ed
UsagiB4 <binaryBunny@tutanota.com>
2021-08-04 19:28:07 +0600

    Some Tricks in Python

---
+++ Tricks/sub1Concatenation.py
@@ -0,0 +1,8 @@
+variable1 = 'New'
+variable2 = 'Year'
+year = 2020
+print('1', variable1, variable2, year)
+print('2 ' + variable1 + " " + variable2 + " " + str(year))
+print("3 %s %s %d" % (variable1, variable2, year))
+print('4 {} {} {}'.format(variable1, variable2, year))
+print(f'5 {variable1} {variable2} {year}')
