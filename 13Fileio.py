f660aaffd5b51eb2f607fea3a310e2e912a71e64
UsagiB4 <binaryBunny@tutanota.com>
2021-08-30 00:17:29 +0600

    File I/O

--- 13Fileio.py
+++ 13Fileio.py
@@ -15,4 +15,26 @@ print(lineList)
 
 a.close()  # you should close after opening a file
 
+# Writing a file. if you open a file in 'w' mode,
+# you will be able to write into the file.
+# But remember, this will delete all the previous content in the file and add the new content(s)
+filew = open('sample.txt', 'w')
+filew.write('a new line')
+filew.close()
 
+# Appending in a file using python
+# 'w' mode removes the previous contents from the file. To stop that you have to use append mode 'a'.
+# this will add new content keeping the previous contents.
+
+
+fileA = open('sample.txt', 'a')
+fileA.write('Appending a new line\n')
+fileA.write('another new line')
+fileA.close()
+
+# Creating a new file using python
+# open('generate.txt', 'x')  # this will create a new text file called generate.txt
+
+gen = open('generate.txt', 'a')
+gen.write('adding new line\n')
+gen.close()
