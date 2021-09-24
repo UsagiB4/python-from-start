23517988a4d0876ec092a51d1c1c2b9f1a240383
UsagiB4 <binaryBunny@tutanota.com>
2021-09-24 16:09:38 +0600

    Magic Methods

--- Tricks/sub2MagicMathodes.py
+++ Tricks/sub2MagicMathodes.py
@@ -18,3 +18,15 @@ self_obj = MyClass(2)
 print(self_obj.sqr())
 del self_obj  # this will delete the object
 #  print(self_obj.add_one())
+
+
+#____________ Comparison Magic Methods ____________
+num1 = 23
+num2 = 4
+
+print(num1.__eq__(num2))  # checks if 2 variables are equal
+print(num1.__ne__(num2))  # same as not equal or !=
+print(num2.__lt__(num1))  # less than
+print(num1.__gt__(num2))  # greater than
+print(num1.__le__(num2))  # less than or equal
+print(num1.__ge__(num2))  # greater than or equal
