d6ff1b129d53ff227700efa9c8b31a2bd6711564
UsagiB4 <binaryBunny@tutanota.com>
2021-09-26 01:39:54 +0600

    2D list

---
+++ Tricks/sub3twoDlist.py
@@ -0,0 +1,6 @@
+food = ['banana', 'rice', 'milk', 'coke', 'water']
+color = ['red', 'blue', 'cyan', 'green', 'black', 'white']
+name = ['Robert', 'Jhon', 'Jams', 'Carl', 'Tyson']
+
+allList = [food, color, name]
+print(allList[1][2])  # first value will access the sub_list, second value will access the value of that list.
