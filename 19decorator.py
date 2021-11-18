7584030d59e668f5223663e7d37b722110cb5a0f
UsagiB4 <binaryBunny@tutanota.com>
2021-11-18 15:18:34 +0600

    Decorator

--- 19decorator.py
+++ 19decorator.py
@@ -10,4 +10,22 @@ def print_int(a_function, numb):
     return
 
 
-print_int(get_int_as_str, 12)
+#  print_int(get_int_as_str, 12)
+
+# Decorator is a type of function that extends another functions functionality
+# without modifying it.
+
+def a_decorator(a_func):
+    def add_hello():
+        str_to_add = "Hello"
+        a_func(str_to_add)
+
+        return
+    return add_hello()
+
+
+@a_decorator
+def say_name(a):
+    print(a,"Tasif")
+    return
+
