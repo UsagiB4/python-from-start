f24a0628034b7b101cdb55dfccaa261aed1939a5
UsagiB4 <binaryBunny@tutanota.com>
2022-01-05 00:09:35 +0600

    sys.path

---
+++ 21x1sysModule.py
@@ -0,0 +1,9 @@
+import sys
+# sys.path returns a list of paths for python. On top it contains the base file (where the script is located) path.
+print(sys.path)
+#
+# we can add new path using sys.path.append('/path/location')
+"""sys.path.append('D:\Projects\pythonRev\Tricks')
+for p in sys.path:
+    print(p)"""
+# appended path will be at the end of the list.
