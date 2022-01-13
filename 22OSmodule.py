95d29079c2887bc0562e43b78367e5a826481ea2
UsagiB4 <binaryBunny@tutanota.com>
2022-01-13 15:14:38 +0600

    getcwd, mkdir

--- 22OSmodule.py
+++ 22OSmodule.py
@@ -2,6 +2,15 @@ import os
 # Operating system Module.
 print(os.getcwd())  # this will show you your current working directory.
 print("Type your dir path: ")
-dirP = input()
+dirP = input()  # asking for directory path.
 os.chdir(dirP)  # this will change your current working directory to the given path.
 print(os.getcwd())
+makeDir = input("Do you wanna make a folder? Y/N ")
+if makeDir.upper() == 'Y':
+    print("Type the name: ")
+    folder_name = input()
+    os.mkdir(folder_name)
+elif makeDir.upper() == 'N':
+    print("Exiting without error")
+else:
+    print("value error")
