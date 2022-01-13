ba19915145e9da2b5a3b5a302fd705923202961a
UsagiB4 <binaryBunny@tutanota.com>
2022-01-13 16:09:48 +0600

    rename, remove, rmdir

--- 22x1OSmodule.py
+++ 22x1OSmodule.py
@@ -1,5 +1,13 @@
 import os
 
-os.chdir('F:')
-os.mkdir('newPythonFile')
-os.remove('F:/newPythonFile')
+os.chdir('F:/newPythonFile')  # changing my directory
+# os.mkdir('newPythonFile')
+with open('myfile.txt', 'w') as myfile:  # creating a demo txt file
+    pass
+os.rename('myfile.txt', 'myfile.pdf')  # renaming myfile.txt to myfile.pdf
+
+#os.remove('myfile.pdf')  # removing the myfile.pdf file
+
+os.mkdir('demo')  # creating a demo folder
+os.rmdir('F:/newPythonFile/demo')  # removing demo folder
+
