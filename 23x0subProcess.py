16b837f21837f3b0cefbcbfd1147a7a1aea7fbc8
UsagiB4 <binaryBunny@tutanota.com>
2022-01-27 02:28:06 +0600

    running basic command, output redirecting

--- 23x0subProcess.py
+++ 23x0subProcess.py
@@ -1,2 +1,12 @@
 import subprocess
 subprocess.run(["echo", "Hello world"], shell=True)
+
+ips = subprocess.run(["ipconfig"], capture_output=True, shell=True)  # this will show you the ip. same as `ifconfig` in linuxOS.
+print(ips.stdout)  # this will return weird output. all in one line. to remove that simply add `text=True`.
+# that will stringify the output.
+
+subprocess.run("cd .. & dir", shell=True) # running multiple command in one line. for windows only.
+
+with open('sample.txt', 'w') as f:
+    fileList = subprocess.run(["dir"], stdout=f, text=True, shell=True)  # redirecting the output text to sample.txt
+
