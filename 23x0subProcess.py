bb5ea2b79804ece91a38d77a90f9fcca0b83b6c0
UsagiB4 <binaryBunny@tutanota.com>
2022-01-27 19:34:07 +0600

    subprocess.run

--- 23x0subProcess.py
+++ 23x0subProcess.py
@@ -5,7 +5,7 @@ ips = subprocess.run(["ipconfig"], capture_output=True, shell=True)  # this will
 print(ips.stdout)  # this will return weird output. all in one line. to remove that simply add `text=True`.
 # that will stringify the output.
 
-subprocess.run("cd .. & dir", shell=True) # running multiple command in one line. for windows only.
+subprocess.run("cd .. & dir", shell=True) # running multiple command in one line. ``commands are for windows only``.
 
 with open('sample.txt', 'w') as f:
     fileList = subprocess.run(["dir"], stdout=f, text=True, shell=True)  # redirecting the output text to sample.txt
