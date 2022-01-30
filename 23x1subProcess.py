398caf736e3f7b625d5e1a61439929fc39c468bf
UsagiB4 <binaryBunny@tutanota.com>
2022-01-31 00:44:26 +0600

    shelex, Popen, output.wait()

--- 23x1subProcess.py
+++ 23x1subProcess.py
@@ -2,11 +2,11 @@ import subprocess
 import shlex
 
 myCmd = "cd & dir"
-args = shlex.split(myCmd)
+args = shlex.split(myCmd)  # this will split the string and return a list.
 print(args)
 
 output = subprocess.Popen(args, shell=True)
 
 print(output)
-print(output.wait())  # returns 0 if the process is successful
+print("Output.wait() = ", output.wait())  # returns 0 if the process is successful
 
