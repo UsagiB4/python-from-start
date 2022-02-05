8033b6ffe0e2268dd014b5a88fc53880cc9ab847
UsagiB4 <binaryBunny@tutanota.com>
2022-02-05 15:58:08 +0600

    Popen.communicate()

--- 23x2subProcess.py
+++ 23x2subProcess.py
@@ -2,4 +2,14 @@ import subprocess
 import shlex
 
 # ::::::::::Popen.communicate()::::::::::
-print('ok')
+
+commandInput = input("Enter your command: ")
+args = shlex.split(commandInput)  # here we are splitting the command
+print(args)
+output = subprocess.Popen(args, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)  # executing the command using Popen(). shell=True [ to avoid issues ]
+try:
+    outP = output.communicate(timeout=5)  # program will terminate after 5 seconds.
+    print(outP)
+except TimeoutError:
+    output.kill()  # terminate the program
+    outs = output.communicate()
