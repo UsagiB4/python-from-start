c844ab395dae11595b8dadb24f36dfafee6dca37
UsagiB4 <binaryBunny@tutanota.com>
2022-02-06 16:09:18 +0600

    Basic Date times

--- 24x0dateTime.py
+++ 24x0dateTime.py
@@ -1,10 +1,15 @@
 from datetime import *
 currentDate = date.today()  # time format >>> yyyy-mm-dd
-print(currentDate)
-print(currentDate.year)  # returns current year
-print(currentDate.day)  # returns current day
+#print(currentDate)
+#print(currentDate.year)  # returns current year
+#print(currentDate.day)  # returns current day
 
 
 #____ change your current date____
 currentDate = currentDate.replace(1999, 3, 30)
-print(currentDate)
+#print(currentDate)
+
+#_____weekday_____
+toDay = date.today()
+wDay = toDay.weekday()  # in python days are numbered as Monday 0 - Sunday 6
+print(wDay)
